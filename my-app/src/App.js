e00493d574cad086c520da4bb222f989b89bcb03
Igor Polonskiy <barlogfire@ya.ru>
2021-09-28 16:33:58 +0300

    first 50 minutes of react video

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState } from "react";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import './styles/App.css'
 
 function App() {
+  const [posts, setPosts] = useState([{ id: 1, title: 'Javascript', body: 'Description' },
+  { id: 2, title: 'Javascript2', body: 'Description' },
+  { id: 3, title: 'Javascript3', body: 'Description' },
+  { id: 4, title: 'Javascript4', body: 'Description' },
+  { id: 5, title: 'Javascript5', body: 'Description' }])
+
+  const [title, setTitle] = useState('')
+
+  const addNewPost = (e) => {
+    e.preventDefault()
+    console.log(title)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <MyInput
+          value = {title}
+          onChange = {e => setTitle(e.target.value)}
+          type='text'
+          placeholder='название поста'
+        />
+        <MyInput type='text' placeholder='описание поста' />
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title='список про js' />
     </div>
   );
 }
